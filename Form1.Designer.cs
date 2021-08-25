4850ade8b1817f33ae6d310d5b2bc6c125ac84d7
Egor wsk <kirenko00@mail.ru>
2021-08-25 17:03:18 +0600

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(310, 213);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(600, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 450);
+            this.panel1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Message";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(61, 126);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Show";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(444, 62);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 52);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Show/Hide menu";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker1.WorkerSupportsCancellation = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+            MessageBox.Show("Text");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            this.BackColor = colorDialog1.Color;
+            Properties.Settings.Default.BackColor = colorDialog1.Color;
+            Properties.Settings.Default.Save();
+            
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Properties.Settings.Default.BackColor;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int posY = 0;
+            for(int i = 0; i < numericUpDown1.Value; i++)
+            {
+                TextBox text = new TextBox();
+                text.Location = new System.Drawing.Point(32, 31 + posY);
+                posY += 50;
+                text.Name = "textBox" + i;
+                text.Size = new System.Drawing.Size(118, 20);
+                text.TabIndex = 2;
+                
+                Controls.Add(text);
+                (Controls["textBox" + i] as TextBox).Text = "My number is " + i;
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text.ToString());
+        }
+
+        bool isClicked = false;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            if (!isClicked)
+            {
+                TextBox text = new TextBox();
+                Panel panel = new Panel();
+                Label label = new Label();
+                Button button = new Button();
+                (Controls["panel1"] as Panel).Controls.Clear();
+                Controls.Remove((Controls["panel1"] as Panel));
+                //panel.Controls.Remove(text);
+                //panel.Controls.Remove(button);
+
+                this.Size = new Size(this.Size.Width - 200, this.Size.Height);
+                isClicked = true;
+            }
+            else if(isClicked == true)
+            {
+                TextBox text = new TextBox();
+                Panel panel = new Panel();
+                Label label = new Label();
+                Button button = new Button();
+
+                this.Size = new Size(this.Size.Width + 200, this.Size.Height);
+                label.AutoSize = true;
+                label.Location = new System.Drawing.Point(71, 53);
+                label.Name = "label1";
+                label.Size = new System.Drawing.Size(50, 13);
+                label.TabIndex = 1;
+                label.Text = "Message";
+
+                button.Location = new System.Drawing.Point(61, 126);
+                button.Name = "button2";
+                button.Size = new System.Drawing.Size(75, 23);
+                button.TabIndex = 2;
+                button.Text = "Show";
+                button.UseVisualStyleBackColor = true;
+                button.Click += new System.EventHandler(this.button2_Click_1);
+
+                text.Location = new System.Drawing.Point(0, 84);
+                text.Name = "textBox1";
+                text.Size = new System.Drawing.Size(200, 20);
+                text.TabIndex = 0;
+
+                panel.Dock = System.Windows.Forms.DockStyle.Right;
+                panel.Location = new System.Drawing.Point(600, 0);
+                panel.Name = "panel1";
+                panel.Size = new System.Drawing.Size(200, 450);
+                panel.TabIndex = 2;
+
+                
+                Controls.Add(panel);
+                panel.Controls.Add(button);
+                panel.Controls.Add(label);
+                panel.Controls.Add(text);
+
+                isClicked = false;
+            }
+        }
+    }
+}
